1003774fca58094a83345ed81171ab6278eb155d
Izabela <Izabela@Izabelas-MacBook-Pro.local>
2018-05-27 11:51:28 +0200

    exploding earth

--- main.js
+++ main.js
@@ -15,5 +15,14 @@
  loadSVG("assets/sarah.svg", "div", function(){
      console.log("sarah loaded");
      document.querySelector("#booms").style.display="none";
-     
- });
+     let stuff = document.querySelectorAll(".land path, .water path");
+     console.log(stuff);
+     document.querySelector("svg").addEventListener("click", () => {
+        stuff.forEach((el, i) => {
+            setTimeout(()=>{
+                console.log(i);
+                el.style.transform = `translate(${Math.random()*400-200}px, ${Math.random()*400-200}px)`;
+            }, i*10)
+        })
+    })
+});
